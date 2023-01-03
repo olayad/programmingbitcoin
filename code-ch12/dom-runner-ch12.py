989f4185c59306146eed7e1d7b20720ca147ded2
Daniel Olaya <daniel.olayam@gmail.com>
2023-01-03 12:23:43 -0800

    wip. ch.12

--- code-ch12/dom-runner-ch12.py
+++ code-ch12/dom-runner-ch12.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
-from importlib import reload
-from helper import run
-import block
-import ecc
-import helper
-import network
-import merkleblock
-import script
-import tx
+from bloomfilter import BloomFilter, BIP37_CONSTANT
+from helper import bit_field_to_bytes, murmur3
 
-run(merkleblock.MerkleBlockTest('test_parse'))
+field_size = 10
+function_count = 5
+tweak = 99
+items = (b'Hello World',  b'Goodbye!')
+bitfield_size = field_size * 8
+bit_field = [0] * bitfield_size
+for phrase in items:
+    for i in range (function_count):
+        seed = i * BIP37_CONSTANT + tweak
+        h = murmur3(phrase, seed=seed)
+        bit = h % bitfield_size
+        bit_field[bit] = 1
+print(bit_field_to_bytes(bit_field).hex())
